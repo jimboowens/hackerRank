2d03bd2ae330b10292d7e1d9d787c6f432369bf6
jimboowens <jimcowens@gmail.com>
2019-03-29 14:24:56 -0400

    for some reason git isn't registering these pushes

--- 10_Days_JavaScript/Day_4.js
+++ 10_Days_JavaScript/Day_4.js
@@ -14,3 +14,18 @@
 // function getCount(objects) {
 //     return objects.filter(function (o) { return o.x == o.y }).length
 // }
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides
+    }
+    perimeter() {
+        return this.sides.reduce((a, b)=>{ return a + b; })
+    }
+}
